7e85765c1df36b3441355a0147754bbe568412af
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-01-19 20:35:18 -0300

    feat: add Header and Footer

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/layout/header/index'
+import Footer from './components/layout/footer/index'
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ web/src/components/layout/header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default () =>(
+    <header>
+        <h1>
+            <img alt="Sprites"/>
+        </h1>
+        <nav>
+            <li>Home</li>
+            <li>Forum</li>
+            <li>Seach</li>
+            <li>Community</li>
+        </nav>
+    </header>
+)
