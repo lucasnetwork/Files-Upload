32e166fd383b5528a4f75c047f28999e178a5a2b
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-01-23 19:59:16 -0300

    feat(web:components aside and header): add context to buttonToggle, add asideBar

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import Header from './components/layout/header/index'
-import Footer from './components/layout/footer/index'
-function App() {
+import React,{useState} from 'react';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import './styles/app.scss'
+
+import {Toggle} from './context'
+
+import {Aside,Footer,Header} from './components/layout'
+import Main from './components/pages/Main'
+import Itens from './components/pages/Itens'
+function App() {  
+  const [visible,setVisible] = useState(false)
   return (
     <>
-      <Header />
-
-      <Footer />
+    <Router>
+      <Toggle.Provider value={{visible,setVisible}}>
+        <Aside />
+        <Header />
+      </Toggle.Provider>
+          <main className="main">
+              <Switch>
+                  <Route path="/" exact component={Main}/>
+                  <Route path="/itens" exact component={Itens}/>
+              </Switch>
+          </main>
+          <Footer />
+    </Router>
     </>
   );
 }
