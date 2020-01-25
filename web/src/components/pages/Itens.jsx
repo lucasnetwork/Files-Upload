dc54449e75f18b808cbe10c30bf6af1dd2afeb82
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-01-25 19:21:15 -0300

    fix(web): add params to request get

--- web/src/components/pages/Itens.jsx
+++ web/src/components/pages/Itens.jsx
@@ -1,18 +1,21 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useContext} from 'react'
 import axios from 'axios'
+import {Toggle} from '../../context'
+import '../../styles/itens.scss'
 function Itens(){
     const [sprites,setSprites] = useState([])
+    const {generation} = useContext(Toggle)
     useEffect(() =>{
         async function getSprites(){
-            const sprites = await axios.get("http://localhost:3333/sprite")
+            const sprites = await axios.get("http://localhost:3333/sprite",{params:{name:generation}})
             setSprites([...sprites.data])
         }
         getSprites()
-    },[])
+    },[generation])
     return(
         <>
             {sprites.map(sprite => (
-                <article key={sprite._id}>
+                <article className="sprite" key={sprite._id}>
                     <p>{sprite.title}</p>
                     <p>{sprite.author}</p>
                     <p>{sprite.generation}</p>
