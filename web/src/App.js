cd1c676bb99e05cdbf27370b7018c7337376e878
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-01-28 20:11:31 -0300

    improvement(web): add components to App.js

--- web/src/App.js
+++ web/src/App.js
@@ -7,22 +7,29 @@ import {Toggle} from './context'
 import {Aside,Footer,Header} from './components/layout'
 import Main from './components/pages/Main'
 import Itens from './components/pages/Itens'
+import Adm from './components/pages/Adm'
+
 function App() {  
+  const [generation,setGeneration] = useState("")
   const [visible,setVisible] = useState(false)
   return (
     <>
-    <Router>
-      <Toggle.Provider value={{visible,setVisible}}>
+    <Router >
+      <Toggle.Provider value={{visible,setVisible,generation,setGeneration}}>
         <Aside />
-        <Header />
+        <div id="visible" 
+            onClick={() =>setVisible(false)}
+            className={`${(visible)?"visibles":"avisible"}`} ></div>
+          <Header />
+            <main  className="main" >
+                <Switch>
+                    <Route path="/" exact component={Main}/>
+                    <Route path="/itens" exact component={Itens}/>
+                    <Route path="/adm" exact component={Adm}/>
+                </Switch>
+            </main>
+            <Footer />
       </Toggle.Provider>
-          <main className="main">
-              <Switch>
-                  <Route path="/" exact component={Main}/>
-                  <Route path="/itens" exact component={Itens}/>
-              </Switch>
-          </main>
-          <Footer />
     </Router>
     </>
   );
