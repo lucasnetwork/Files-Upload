576c1eff758691837a2b84e6c3ca07d33c60f44c
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-01-28 19:59:03 -0300

    improvement(web:itens): i have added the connection to the backend

--- web/src/components/pages/Itens.jsx
+++ web/src/components/pages/Itens.jsx
@@ -1,5 +1,5 @@
 import React,{useState,useEffect,useContext} from 'react'
-import axios from 'axios'
+import {api} from './../../services/api'
 import {Toggle} from '../../context'
 import '../../styles/itens.scss'
 function Itens(){
@@ -7,21 +7,22 @@ function Itens(){
     const {generation} = useContext(Toggle)
     useEffect(() =>{
         async function getSprites(){
-            const sprites = await axios.get("http://localhost:3333/sprite",{params:{name:generation}})
-            setSprites([...sprites.data])
+            const sprites = await api.get("sprite",{params:{name:generation}})
+            setSprites(sprites.data)
         }
         getSprites()
     },[generation])
     return(
-        <>
-            {sprites.map(sprite => (
-                <article className="sprite" key={sprite._id}>
+        <div className="sprite">
+            {sprites.map(sprite =>(
+                <article  key={sprite._id}>
+                    <img src={`http://localhost:3333/sprite/${sprite.img}`} alt={sprite.title} />
                     <p>{sprite.title}</p>
                     <p>{sprite.author}</p>
                     <p>{sprite.generation}</p>
                 </article>
             ))}
-        </>
+        </div>
     )
 }
 export default Itens
