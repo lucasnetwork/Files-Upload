c470292b20c3d075ead65e7ba92e6f1196e70dae
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-01-28 20:01:45 -0300

    refactor(web:main): d

--- web/src/components/pages/Main.jsx
+++ web/src/components/pages/Main.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
 export default () =>(
-    <article>
-        <h1>Sobre</h1>
-        <p>
-            This website is a part of several websites under the belt of "The VG Resource" - websites that are dedicated to the collection, archival, 
-            and appreciation of materials from video games.
-        </p>
-    </article>
+    <>
+        <article className="article-main">
+            <h1>Sobre</h1>
+            <p>
+                This website is a part of several websites under the belt of "The VG Resource" - websites that are dedicated to the collection, archival, 
+                and appreciation of materials from video games.
+            </p>
+        </article>
+        <article className="article-main">
+            <p>
+                we have many sprites of very generations of consoles
+            </p>
+            <h1>Collection</h1>
+        </article>
+    </>
 )
