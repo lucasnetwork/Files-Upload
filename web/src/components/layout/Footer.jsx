22bd7699ee83262060e79204863b8a3592c7006f
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-03-15 18:24:31 -0300

    improvement(web): add links of social redes in the footer

--- web/src/components/layout/Footer.jsx
+++ web/src/components/layout/Footer.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTwitter,faGithub,faLinkedin} from '@fortawesome/free-brands-svg-icons'
 import '../../styles/footer.scss'
 export default () => (
     <footer className="footer">
-        <p>Copyright 2003-2020, The VG Resource</p>
+        <p>Copyright 2019-2020, Data Files</p>
         <aside>
             <Link>
                 <li>wiki</li>
             </Link>
             <Link>
-                <li>sound</li>
-            </Link>
-            <Link>
                 <li>models</li>
             </Link>
+            <a href="https://twitter.com/LucasRi25613630"><FontAwesomeIcon icon={faTwitter}/></a>
+            <a href="https://github.com/lucasnetwork"><FontAwesomeIcon icon={faGithub}/></a>
+            <a href="https://www.linkedin.com/in/lucas-ribeiro-22338b178/"><FontAwesomeIcon icon={faLinkedin}/></a>
         </aside>
     </footer>
 )
